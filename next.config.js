57c6f619c5b9850d059d2b115a2137f094a4a5fe
Ayrton <modz.ayrton@gmail.com>
2022-12-15 13:41:40 +0000

    Update 4 files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "images.habbo.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
